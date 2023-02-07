3c83833ed7cdc1651f89055d9c0487325da379f6
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2023-02-07 22:33:56 +0300

    Fix input file path

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const fs = require('fs');
-const core = require('@actions/core');
-const puppeteer = require('puppeteer');
+const { writeFileSync } = require('fs');
+const { cwd } = require('process');
+const { join } = require('path');
+const { getInput } = require('@actions/core');
+const { launch } = require('puppeteer');
 
 (async () => {
     
-    const inputFile = core.getInput('path');
-    const outputFile = core.getInput('output');
+    const inputFile = join(cwd(), getInput('path'));
+    const outputFile = getInput('output');
 
     console.log(`Input file: ${inputFile}`);
     console.log(`Output file: ${outputFile}`);
 
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const browser = await launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.goto(`file://${inputFile}`, { waitUntil: "networkidle0" });
 
     const pdf = await page.pdf();
     console.log("PDF generated, writing to the file")
 
-    fs.writeFileSync(outputFile, pdf);
+    writeFileSync(outputFile, pdf);
 
     console.log("Done");
 })();
