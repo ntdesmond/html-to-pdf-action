3b416e4924e1b603ee92686df195d61e54b158d5
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2023-07-06 12:20:35 +0300

    Enable fetching remote files

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
-const { promises: { mkdir, writeFile } } = require('fs');
-const { cwd } = require('process');
-const { join, dirname } = require('path');
-const { getInput } = require('@actions/core');
-const { launch } = require('puppeteer');
+const {
+  promises: { mkdir, writeFile },
+} = require("fs");
+const { cwd } = require("process");
+const { join, dirname } = require("path");
+const { getInput } = require("@actions/core");
+const { launch } = require("puppeteer");
 
 (async () => {
-    
-    const inputFile = join(cwd(), getInput('path') || './cv.html');
-    const outputFile = getInput('output') || './test/path/cv.pdf';
-    const pdfOptions = JSON.parse(getInput('options') || "{}");
+  let inputPath =
+    getInput("path") || "http://host.docker.internal:4173/cv.html";
+  if (!/^.+:\/\//.test(inputPath)) {
+    inputPath = `file://${join(cwd(), inputPath)}`;
+  }
 
-    console.log(`Input file: ${inputFile}`);
-    console.log(`Output file: ${outputFile}\n`);
+  const outputFile = getInput("output") || "./test/path/cv.pdf";
+  const pdfOptions = JSON.parse(getInput("options") || "{}");
 
-    console.log("Making sure output directory exists");
-    const dir = dirname(outputFile);
-    await mkdir(dir, { recursive: true });
+  console.log(`Input path: ${inputPath}`);
+  console.log(`Output file: ${outputFile}\n`);
 
-    console.log("Opening the browser")
-    const browser = await launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
-    
-    const url = `file://${inputFile}`;
-    console.log(`Navigating to ${url}`)
-    await page.goto(url, { waitUntil: "networkidle0" });
+  console.log("Making sure output directory exists");
+  const dir = dirname(outputFile);
+  await mkdir(dir, { recursive: true });
 
-    const pdf = await page.pdf(pdfOptions);
-    
-    console.log(`PDF is ready, writing to ${outputFile}`)
-    await writeFile(outputFile, pdf);
-    
-    console.log("Closing the browser")
-    await browser.close()
+  console.log("Opening the browser");
+  const browser = await launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  const page = await browser.newPage();
 
-    console.log("Done");
+  console.log(`Navigating to ${inputPath}`);
+  await page.goto(inputPath, { waitUntil: "networkidle0" });
+
+  const pdf = await page.pdf(pdfOptions);
+
+  console.log(`PDF is ready, writing to ${outputFile}`);
+  await writeFile(outputFile, pdf);
+
+  console.log("Closing the browser");
+  await browser.close();
+
+  console.log("Done");
 })();
