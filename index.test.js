18f7cc1ed2b11f39d5619d722537aefa889c33ad
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2023-07-07 14:42:10 +0300

    Add tests

---
+++ index.test.js
@@ -0,0 +1,73 @@
+const { describe, it, expect } = require("@jest/globals");
+const child_process = require("child_process");
+const path = require("path");
+const { createPdf, pathToUrl } = require(".");
+
+const script_path = path.join(__dirname, "index.js");
+const runAction = (env) => {
+  const { status, stderr, stdout } = child_process.spawnSync(
+    "node",
+    [script_path],
+    { env }
+  );
+  console.log(
+    `Action exited with status code ${status}.\n` +
+      `Stdout:\n${stdout.toString()}` +
+      `Stderr:\n${stderr.toString()}`
+  );
+  return status;
+};
+
+describe("createPdf", () => {
+  const outputFile = "file.pdf";
+  const pdfOptions = {};
+  it("fails on non-existent input path", async () => {
+    await createPdf({
+      inputPath: pathToUrl("not-a-file.html"),
+      outputFile,
+      pdfOptions,
+    }).catch(() => {});
+  });
+  it("fails on invalid input path", async () => {
+    return createPdf({
+      inputPath: "what/is\\this",
+      outputFile,
+      pdfOptions,
+    }).catch(() => {});
+  });
+  it("succeeds on existing local files", async () => {
+    await createPdf({
+      inputPath: pathToUrl("file.html"),
+      outputFile,
+      pdfOptions,
+    });
+  });
+  it("succeeds on files served over HTTP", async () => {
+    await createPdf({
+      inputPath: "http://127.0.0.1:8080/file.html",
+      outputFile,
+      pdfOptions,
+    });
+  });
+});
+
+describe("action", () => {
+  const INPUT_PATH = "file.html";
+  const INPUT_OUTPUT = "file.pdf";
+  const INPUT_OPTIONS = "{}";
+  it('fails on missing "path"', async () => {
+    const status = runAction({ INPUT_OUTPUT, INPUT_OPTIONS });
+    expect(status).not.toBeNull;
+    expect(status).not.toBe(0);
+  });
+  it('fails on missing "output"', async () => {
+    const status = runAction({ INPUT_PATH, INPUT_OPTIONS });
+    expect(status).not.toBeNull;
+    expect(status).not.toBe(0);
+  });
+  it('succeeds when "path" and "output" are defined', async () => {
+    const status = runAction({ INPUT_PATH, INPUT_OUTPUT, INPUT_OPTIONS });
+    expect(status).not.toBeNull;
+    expect(status).toBe(0);
+  });
+});
